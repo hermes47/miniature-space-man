95df08c6d4ade52785d69e0f7c791fb67aa2b889
Ivan Welsh <ivan@welsh.co.nz>
2016-02-13 22:48:34 +1300

    Added project to X-Code

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  miniature-space-man
+//
+//  Created by Ivan Welsh on 13/02/16.
+//  Copyright © 2016 Ivan Welsh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
